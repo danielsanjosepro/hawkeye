c09153d14efe4add946c30fd5e952df0d7b2f144
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-02-18 18:56:22 +0100

    Add github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // Required since GitHub Pages does not support Next.js Image Optimization
+  },
+  basePath: "/hawkeye", // Change this to match your repository name
+  assetPrefix: "/hawkeye", /* config options here */
 };
 
 export default nextConfig;
